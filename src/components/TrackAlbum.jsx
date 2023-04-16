7bdc3fdf0734cb3b13354021d6b427a0c906d3a2
boolick <boolick38@gmail.com>
2023-04-16 17:18:19 +0300

    create components, screens, fonts

---
+++ src/components/TrackAlbum.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TrackAlbum() {
+  return (
+    <div className="track__album">
+    <a className="track__album-link" href="http://">Welcome Reality</a>
+</div>
+  )
+}
+
+export default TrackAlbum
