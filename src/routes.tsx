96b692125edf6777f5d8f77d35dc2e14f5a2cdbb
boolick <boolick38@gmail.com>
2023-08-07 18:56:16 +0300

    Fixed routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Main from "./screens/Main";
-import MyPlaylist from "./screens/MyPlaylist";
+import MyPlaylist from "./screens/Myplaylist";
 import LoginPage from "./screens/LoginPage/LoginPage";
 import NotFound from "./screens/NotFoundPage/NotFound";
 import { Track } from "./components/Request/Request";
